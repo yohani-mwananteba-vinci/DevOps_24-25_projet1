a8b2b3b28016159fb424446d437bc30852bf0888
MwanaNteba Yohani <yohani.mwananteba@student.vinci.be>
2024-11-12 19:12:48 +0100

    Refactor: test: the berlin clock has one lights turn on (for 1-4 min)

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -3,35 +3,62 @@ import { Main } from "../src/app.js";
 describe("Berlinclock function should return the clock of a timestamp", function(){
     let main = new Main();
 
-    let now = new Date(0);
+    let now = new Date();
+
+
 
     it("Berlinclock should return a string representing a clock with all lights turn off", function(){
-        let result = main.berlinClock(now.getTime());
+        now.setHours(0);
+        now.setMinutes(0);
+        now.setSeconds(0);
+        now.setMilliseconds(0);
+        let result = main.berlinClock(now);
 
         expect(result).toBe("0000 \n 00000000000 \n 0000 \n 0000 \n 0")
     });
 
     it("Berlinclock should return a string representing a clock with one light turn on after 1 min", function(){
-
-        let result = main.berlinClock(now.getTime()+60000);
+        now.setHours(0);
+        now.setMinutes(1);
+        now.setSeconds(0);
+        now.setMilliseconds(0);
+        
+        let result = main.berlinClock(now);
 
         expect(result).toBe("X000 \n 00000000000 \n 0000 \n 0000 \n 0");
     });
 
     
     it("Berlinclock should return a string representing a clock with 2 lights turn on after 2 min", function(){
-
-        let result = main.berlinClock(now.getTime()+(2*60000));
+        now.setHours(0);
+        now.setMinutes(2);
+        now.setSeconds(0);
+        now.setMilliseconds(0);        let result = main.berlinClock(now);
 
         expect(result).toBe("XX00 \n 00000000000 \n 0000 \n 0000 \n 0");
     });
 
         
     it("Berlinclock should return a string representing a clock with 3 lights turn on after 3 min", function(){
-
-        let result = main.berlinClock(now.getTime()+(3*60000));
+        now.setHours(0);
+        now.setMinutes(3);
+        now.setSeconds(0);
+        now.setMilliseconds(0);
+        let result = main.berlinClock(now);
 
         expect(result).toBe("XXX0 \n 00000000000 \n 0000 \n 0000 \n 0");
     });
+
+    
+    it("Berlinclock should return a string representing a clock with 4 lights turn on after 4 min", function(){
+        now.setHours(0);
+        now.setMinutes(4);
+        now.setSeconds(0);
+        now.setMilliseconds(0);
+        let result = main.berlinClock(now);
+
+        expect(result).toBe("XXXX \n 00000000000 \n 0000 \n 0000 \n 0");
+    });
+
 
 });
--- src/app.js
+++ src/app.js
@@ -1,18 +1,21 @@
 export class Main {
 
-    berlinClock(number){
-        let now = new Date(0);
-
-        if(number === now.getTime())
-            return "0000 \n 00000000000 \n 0000 \n 0000 \n 0"; 
-
-        if (number === now.getTime()+60000)
+    berlinClock(timestamp){
+        let now = new Date();
+        if (timestamp.getMinutes() === 1)
              return "X000 \n 00000000000 \n 0000 \n 0000 \n 0";
 
-        if (number === now.getTime()+(2*60000))
+        if (timestamp.getMinutes() === 2)
             return "XX00 \n 00000000000 \n 0000 \n 0000 \n 0";
 
-        if (number === now.getTime()+(3*60000))
+        if (timestamp.getMinutes() === 3)
             return "XXX0 \n 00000000000 \n 0000 \n 0000 \n 0";
+
+        if (timestamp.getMinutes() === 4)
+            return "XXXX \n 00000000000 \n 0000 \n 0000 \n 0";
+
+            return "0000 \n 00000000000 \n 0000 \n 0000 \n 0"; 
+
     }
+
 }
