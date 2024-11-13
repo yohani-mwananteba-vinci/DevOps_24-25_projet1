9e3256ccb4480ae99aefe48d7faafd33252b8945
Gaetan Van den Berghe <gaetan.vandenberghe@student.uclouvain.be>
2024-11-13 16:52:52 +0100

    Refactor: first and seconde function

--- src/app.js
+++ src/app.js
@@ -1,128 +1,91 @@
 export class Main {
+  updateFisrtRow(timestamp) {
+    let now = new Date();
+    let result = "";
 
-    berlinClock(timestamp){
-        let now = new Date();
-        let result = "";
-
-        const singleMinutes = timestamp.getMinutes() % 5;
-        if(singleMinutes === 0){
-            result +="0000 \n"
-        }
-
-        if (singleMinutes === 1){
-            result +="X000 \n"
-        }
-            
-        if (singleMinutes === 2){
-            result +="XX00 \n"
-        }
-
-        if (singleMinutes === 3){
-            result +="XXX0 \n"
-        }
-
-        if (singleMinutes === 4){
-            result +="XXXX \n"
-        }
-
-        const fiveMinuteBlocks = Math.floor(timestamp.getMinutes() / 5);
-
-        if(fiveMinuteBlocks === 0){
-            result +=" 00000000000 \n"
-        }
-
-        if (fiveMinuteBlocks === 1){
-            result +=" X0000000000 \n"
-        }
-
-        if (fiveMinuteBlocks === 2){
-            result +=" XX000000000 \n"
-        }
-
-        if (fiveMinuteBlocks === 3){
-            result +=" XXX00000000 \n"
-        }
-
-        if (fiveMinuteBlocks === 4){
-            result +=" XXXX0000000 \n"
-        }
-
-        if (fiveMinuteBlocks === 5){
-            result +=" XXXXX000000 \n"
-        }
-
-        if (fiveMinuteBlocks === 6){
-            result +=" XXXXXX00000 \n"
-        }
-
-        if (fiveMinuteBlocks === 7){
-            result +=" XXXXXXX0000 \n"
-        }
-
-        if (fiveMinuteBlocks === 8){
-            result +=" XXXXXXXX000 \n"
-        }
-
-        if (fiveMinuteBlocks === 9){
-            result +=" XXXXXXXXX00 \n"
-        }
-
-        if (fiveMinuteBlocks === 10){
-            result +=" XXXXXXXXXX0 \n"
-        }
-
-        if (fiveMinuteBlocks === 11){
-            result +=" XXXXXXXXXXX \n"
-        }
-
-        const singleHours = timestamp.getHours() % 5;
-
-        if (singleHours === 0){
-            result += " 0000 \n"
-        }
-
-        if (singleHours === 1){
-            result += " X000 \n"
-        }
-
-        if (singleHours === 2){
-            result += " XX00 \n"
-        }
-
-        if (singleHours === 3){
-            result += " XXX0 \n"
-        }
-
-        if (singleHours === 4){
-            result += " XXXX \n"
-        }
-
-        const fiveHourBlocks = Math.floor(timestamp.getHours() / 5);
-
-        if (fiveHourBlocks === 0){
-            result += " 0000 \n"
-        }
-
-        if (fiveHourBlocks === 1){
-            result += " X000 \n"
-        }
-
-        if (fiveHourBlocks === 2){
-            result += " XX00 \n"
-        }
-
-        if (fiveHourBlocks === 3){
-            result += " XXX0 \n"
-        }
-
-        if (fiveHourBlocks === 4){
-            result += " XXXX \n"
-        }
-
-        result+=" 0"
-
-            return result; 
-    
+    const lamp_allume = timestamp.getMinutes() % 5;
+    return "X".repeat(lamp_allume).padEnd(4, 0);
+  }
+  updateSecondRow(timestamp) {
+    let now = new Date();
+    let result = "";
+    const lampeAllume = Math.floor(timestamp.getMinutes() / 5);
+    for (let i = 1; i < 11; i++) {
+      if (i < lampeAllume) {
+        if (i % 3 == 0) result += "Y";
+        else result += "X";
+      } else {
+        result += "0";
+      }
     }
 
+    return result;
+  }
+  updateThirdRow(timestamp) {
+    let now = new Date();
+    let result = "";
+    const singleHours = timestamp.getHours() % 5;
+
+    if (singleHours === 0) {
+      result += " 0000 \n";
+    }
+
+    if (singleHours === 1) {
+      result += " X000 \n";
+    }
+
+    if (singleHours === 2) {
+      result += " XX00 \n";
+    }
+
+    if (singleHours === 3) {
+      result += " XXX0 \n";
+    }
+
+    if (singleHours === 4) {
+      result += " XXXX \n";
+    }
+    return result;
+  }
+  updateFourthRow(timestamp) {
+    let now = new Date();
+    let result = "";
+    const fiveHourBlocks = Math.floor(timestamp.getHours() / 5);
+
+    if (fiveHourBlocks === 0) {
+      result += " 0000 \n";
+    }
+
+    if (fiveHourBlocks === 1) {
+      result += " X000 \n";
+    }
+
+    if (fiveHourBlocks === 2) {
+      result += " XX00 \n";
+    }
+
+    if (fiveHourBlocks === 3) {
+      result += " XXX0 \n";
+    }
+
+    if (fiveHourBlocks === 4) {
+      result += " XXXX \n";
+    }
+
+    result += " 0";
+
+    return result;
+  }
+
+  testFunction() {
+    let today = new Date();
+    return {
+      FirstRow: this.updateFisrtRow(today),
+      SecondRow: this.updateSecondRow(today),
+      ThirdRow: this.updateThirdRow(today),
+    };
+  }
 }
+
+let main = new Main();
+console.log(main.testFunction());
